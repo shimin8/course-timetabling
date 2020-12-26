587f509bfc783888a585e8feb16556d837f2f22d
shimin8 <nimish007gupta@gmail.com>
2020-12-26 14:27:53 +0530

    display function

--- GA.cpp
+++ GA.cpp
@@ -15,13 +15,27 @@ const string MAP="yxxzyx";
 const int SUB[SUBJECTS_SIZE]={7,3,6,4,5,5};
 const int PROF[PROFESSORS_SIZE]={13,12,5};
 
-class Individual {
+class Individual
+{
     public:
 
     string chromosome;
     int fitness;
 };
 
-int main() {
+void display(vector <Individual> population)
+{
+	int l=PERIODS*DAYS;
+	for(int i=0;i<l;i++)
+	{
+		if(i%PERIODS==0)
+			cout<<endl;
+		cout<<'\t'<<population[0].chromosome[i];
+	}
+	cout<<"\n\tFitness : "<<population[0].fitness<<endl;
+}
+
+int main()
+{
 	vector<Individual> population;
 }
